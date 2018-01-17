b49a1e62be52343fc365abc44dfd1ed704c285fc
caitlnt <31721327+caitlnt@users.noreply.github.com>
2018-01-16 19:41:24 -0800

    HW-08 updated

--- HW-08/CTa-HW08-SQL.sql
+++ HW-08/CTa-HW08-SQL.sql
@@ -168,7 +168,7 @@ and title like 'K%'or title like 'Q%';
 select first_name, last_name
 from actor
 where actor_id in 
-(select actor_id from film_actor where film_id in
+(select actor_id from film_actor where film_id =
 (select film_id from film where title = 'Alone Trip'));
 
 
